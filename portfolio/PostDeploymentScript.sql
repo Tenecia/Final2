2be753a9b8cdbb37eceb7e77dc11c1bf6a780ff5
Tenecia <aja.guinea@gmail.com>
2015-11-26 00:25:27 -0500

    Final2

---
+++ portfolio/PostDeploymentScript.sql
@@ -0,0 +1,24 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+--MERGE INTO Projects AS Target
+--USING (VALUES 
+--        (1, 'Janet Jackson', 'Janet Jackson Tour Page', '~/App_Data/images/LinkedInPhoto.jpg', 'githublink')
+--		(2,'Passion Project','Tiny House B&B','~/App_Data/images/LinkedInPhoto.jpg','githublink')
+--		(3,'T-Shirt Shop','T-Shirt Shop places orders for customers','~/App_Data/images/LinkedInPhoto.jpg','githublink')
+--		(4,'MidTerm','Inventory tracker for Movie Store','~/App_Data/images/LinkedInPhoto.jpg','githublink')
+--		(4,'Majic 8 Ball','Majic 8 Ball pharses','~/App_Data/images/LinkedInPhoto.jpg','githublink')
+--)
+--AS Source (Id, Name, ProjectDescription, Screenshot, GitHub)
+--ON Target.ID = Source.ID
+--WHEN NOT MATCHED BY TARGET THEN
+--INSERT ( Name, ProjectDescription, Screenshot, GitHub)
+--VALUES ( Name, ProjectDescription, Screenshot, GitHub);
---
+++ portfolio/projects.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[projects]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(MAX) NULL, 
+    [ProjectDescription] NVARCHAR(50) NULL, 
+    [Screenshot] NVARCHAR(MAX) NULL, 
+    [GitHub] NVARCHAR(MAX) NULL
+)
